3338089335fc689662414d925826cce5466869df
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2025-04-11 22:33:12 +0300

    9_2545

--- home_9_1.py
+++ home_9_1.py
@@ -4,3 +4,4 @@ def popular_words (text, words):
 
 assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
 print('OK')
+#gvfdhdtfhjtjur
